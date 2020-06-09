f63263832a35386999dc98908022bd1cdb026643
Luis <luismazgz@gmail.com>
2020-06-09 13:45:20 +0200

    organizacion en módulos y rutas

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
-  templateUrl: './pages.component.html',
+  templateUrl: 'pages.component.html',
   styles: [
   ]
 })
